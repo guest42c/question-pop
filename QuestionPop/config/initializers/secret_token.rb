e53cb7b58d9a2f0f5bd0b36b314925a78ac20efb
Guilherme Araujo <gui2811@gmail.com>
2012-08-17 18:27:06 -0300

    Create rails app

---
+++ QuestionPop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuestionPop::Application.config.secret_token = '4a28523299ce41fb7f4f9ae7456adbea03a1e864a6480a70a96ecb4e7ec7c7db8c1067c54383735aae47526922dc104d00f3b96c2f2a548df09156abce9208dd'
